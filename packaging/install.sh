fe39df45b5bf95c88682c673f6f35384c3e8666d
actor <gizatullininsaf25@gmail.com>
2026-07-10 14:29:03 +0300

    fix: isolate desktop staging from dependency builds

--- packaging/install.sh
+++ packaging/install.sh
@@ -2,13 +2,14 @@
 set -eu
 
 prefix=${PREFIX:-/usr/local}
-destdir=${DESTDIR:-}
+install_root=${DESTDIR:-}
+unset DESTDIR
 
 cargo build --release
-install -Dm755 target/release/rode "$destdir$prefix/bin/rode"
+install -Dm755 target/release/rode "$install_root$prefix/bin/rode"
 install -Dm644 packaging/dev.rode.Rode.desktop \
-  "$destdir$prefix/share/applications/dev.rode.Rode.desktop"
+  "$install_root$prefix/share/applications/dev.rode.Rode.desktop"
 install -Dm644 packaging/dev.rode.Rode.svg \
-  "$destdir$prefix/share/icons/hicolor/scalable/apps/dev.rode.Rode.svg"
+  "$install_root$prefix/share/icons/hicolor/scalable/apps/dev.rode.Rode.svg"
 install -Dm644 packaging/dev.rode.Rode.metainfo.xml \
-  "$destdir$prefix/share/metainfo/dev.rode.Rode.metainfo.xml"
+  "$install_root$prefix/share/metainfo/dev.rode.Rode.metainfo.xml"
